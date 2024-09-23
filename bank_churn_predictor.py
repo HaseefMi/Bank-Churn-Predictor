18870f159b77cec6854964034fa9a17333eb8c63
Haseef Minhas <haseefminhas9@gmail.com>
2024-09-23 11:38:49 -0400

    Uploaded Training Set & Deep Learning Model Script

---
+++ bank_churn_predictor.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import keras as ker
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+dataset = pd.read_csv("C:/Users/hacho/Downloads/Machine-Learning/Artifical-Neural-Network/bank_churn_venv/Churn_Modelling.csv")
+X = dataset.iloc[:, 3:-1].values
+y = dataset.iloc[:, -1].values
+
+le = LabelEncoder()
+X[:, 2] = le.fit_transform(X[:, 2])
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+ann = ker.models.Sequential()
+ann.add(ker.layers.Dense(units=6, activation='relu'))
+ann.add(ker.layers.Dense(units=6, activation='relu'))
+ann.add(ker.layers.Dense(units=1, activation='sigmoid'))
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+ann.fit(X_train, y_train, batch_size=32, epochs=30)
